b795dd207689843df14ec95c90b236cd1d85b62f
John Lifsey <nebulous@crashed.net>
2023-02-25 08:30:53 -0500

    configure lcd in build flags
    doc tweaks
    start making yaml more generic/configurable

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # InfinitESP
 ESP32 implementation of Carrier thermostat RS485 "ABCD bus"
 
-Placeholder for an [esp32-box-lite](https://amzn.to/3PqKc7E) firmware implementation of Carrier's serial RS485 protocol to automate Infinity Touch thermostats.
+Placeholder for an esp32 firmware implementation of Carrier's serial RS485 protocol to automate Infinity Touch thermostats.
+
+Prototype is being built on the esp32-s3-box-lite
+[esp32-box-lite](https://amzn.to/3PqKc7E) 
 
 <a href="https://amzn.to/3PqKc7E">
   <img src="https://user-images.githubusercontent.com/177510/207661664-22b4837d-06b4-430d-8025-3dc1fda6cab4.png">
@@ -14,19 +17,11 @@ Placeholder for an [esp32-box-lite](https://amzn.to/3PqKc7E) firmware implementa
 
 1. Parse incoming RS485 state
 2. Control state (current setpoints & schedule) via serial writes, similar to what the [Infinitive](https://github.com/acd/infinitive) project does for legacy stats
-3. Provide basic LCD thermostat interface
-probably based on the [LVGL](https://github.com/lvgl/lvgl) thermostat example
-
-![image](https://user-images.githubusercontent.com/177510/207666737-81bc9be4-353f-4c83-9145-7f2431edc431.png)
-
-4. Implement a Home Assistant standard Climate device.
-   * Either as an MQTT climate device 
-   * or a custom climate device. 
-   * Ideally as an [ESPHome](https://esphome.io) integration
-6. OpenSCAD / STL file to contain necessary RS485 module.
-7. Autoconfigure wifi since the thermostat freely gives out wifi credentials 🤯
-8. Provide web interface
-9. Play nicely with [Infinitude](https://github.com/lvgl/lvgl)
+3. Implement a Home Assistant Climate device via [ESPHome](https://esphome.io)
+4. Provide basic LCD thermostat interface
+    * potentially a spin-off Home Assistant physical thermostat interface
+6. Autoconfigure wifi since the thermostat freely gives out wifi credentials 🤯
+7. Provide web interface
+8. Play nicely with [Infinitude](https://github.com/lvgl/lvgl)
    * Act as serial/tcp bridge
    * Serve as NAT gateway
-1. ESP Rainmaker app/cloud interface
--- tftespi.h
+++ tftespi.h
@@ -34,27 +34,29 @@ class MyTFTeSPI : public Component {
 
                 // draw setpoint arc
                 int r = cy-10;
-                tft.drawArc(      cx, cy, r+1, r-arcwidth-1,   40+(setpoint*5), 40, TFT_BLACK, 0, false);
-                tft.drawSmoothArc(cx, cy, r,   r-arcwidth, 40, 40+(setpoint*5), TFT_BLUE, 0, false);
+                float scale = 280.0/100.0;
+                tft.drawArc(      cx, cy, r+1, r-arcwidth-1,   40+(setpoint*scale), 40, TFT_BLACK, 0, false);
+                tft.drawSmoothArc(cx, cy, r,   r-arcwidth, 40, 40+(setpoint*scale), TFT_BLUE, 0, false);
 
                 // draw current temperature arc
                 r = cy-30;
-                tft.drawArc(      cx, cy, r+1, r-arcwidth-1, 40+(current*5), 40, TFT_BLACK, 0, false);
-                tft.drawSmoothArc(cx, cy, r+1, r-arcwidth-1, 40, 40+(current*5), TFT_SKYBLUE, 0, false);
+                tft.drawArc(      cx, cy, r+1, r-arcwidth-1, 40+(current*scale), 40, TFT_BLACK, 0, false);
+                tft.drawSmoothArc(cx, cy, r+1, r-arcwidth-1, 40, 40+(current*scale), TFT_SKYBLUE, 0, false);
             }
         }
         void loop() override {
             static int t=millis();
-            static float temperature = id(ds18b20).state;
-            if (id(ds18b20).state != temperature) {
-                temperature = id(ds18b20).state;
+            static float temperature = id(climate_clone_current_temperature).state;
+            static float setpoint = id(climate_clone_setpoint).state;
+            if (id(climate_clone_current_temperature).state != temperature) {
+                temperature = id(climate_clone_current_temperature).state;
+                setpoint = id(climate_clone_setpoint).state;
                 setRefresh();
-                tempArc(30, temperature);
+                tempArc(setpoint, temperature);
             }
             if (millis() > t+60000 || needsRefresh) {
                 needsRefresh = 0;
                 t=millis();
-                temperature = id(ds18b20).state;
 
                 tft.setTextFont(7);
 
@@ -66,8 +68,11 @@ class MyTFTeSPI : public Component {
 
                 if (temperature>-20 and temperature<80) {
                     tft.setCursor(160-32,100);
-                    float f = (float)(32.0+(temperature*1.8));
-                    tft.print( (int)f );
+                    //float f = (float)(32.0+(temperature*1.8));
+                    tft.print( (int)temperature );
+                    tft.setTextFont(4);
+                    tft.setCursor(160-15,150);
+                    tft.println( (int)setpoint );
                     //ESP_LOGD("custom", "tft espi loop");
                 }
 
